b54fba857c8f6c1653ef56af61185d0f9555ea98
funnyhell <sofunnyhell@gmail.com>
2023-09-01 16:39:40 +0300

    Small refactoring in Post model, made show posts from user page

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,5 +1,5 @@
-<a href="{{route('posts.show', $post->slug)}}">
-    <div class="bg-gray-300 dark:bg-gray-600 overflow-hidden shadow-xl sm:rounded-lg m-4 p-2">
+<div class="bg-gray-300 dark:bg-gray-600 overflow-hidden shadow-xl sm:rounded-lg m-4 p-2">
+    <a href="{{route('posts.show', $post->slug)}}">
         <div class="grid gap-6 grid-cols-7 sm:grid-cols-12">
             <h1 class="text-gray-900 dark:text-white col-span-5 sm:col-span-3 font-semibold sm:text-xl">
                 {{$post->title}}
@@ -17,38 +17,37 @@
                 {{__('Comments')}}: {{$post->comment_count}}
             </h1>
         </div>
+    </a>
+    <div class="grid gap-6 grid-cols-2 sm:grid-cols-8 mt-8">
+        @if(!$post->is_published)
+            <form action="{{route('posts.publish')}}" method="POST" class="col-span-1">
+                @csrf
+                <input type="hidden" name="id" value="{{$post->id}}">
+                <input type="submit" value="{{__('Publish')}}"
+                       class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            </form>
+        @else
+            <form action="{{route('posts.unpublish')}}" method="POST" class="col-span-1">
+                @csrf
+                <input type="hidden" name="id" value="{{$post->id}}">
+                <input type="submit" value="{{__('Unpublish')}}"
+                       class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            </form>
+        @endif
+        @if($post->is_deleted)
+            <form action="{{route('posts.restore')}}" method="POST" class="ml-4">
+                @csrf
+                <input type="hidden" name="id" value="{{$post->id}}">
+                <input type="submit" value="{{__('Restore')}}"
+                       class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            </form>
+        @else
+            <form action="{{route('posts.delete')}}" method="POST" class="ml-4">
+                @csrf
+                <input type="hidden" name="id" value="{{$post->id}}">
+                <input type="submit" value="{{__('Delete')}}"
+                       class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            </form>
+        @endif
     </div>
-</a>
-<div class="grid gap-6 grid-cols-2 sm:grid-cols-8 mt-8">
-    @if(!$post->is_published)
-        <form action="{{route('posts.publish')}}" method="POST" class="col-span-1">
-            @csrf
-            <input type="hidden" name="id" value="{{$post->id}}">
-            <input type="submit" value="{{__('Publish')}}"
-                   class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-        </form>
-    @else
-        <form action="{{route('posts.unpublish')}}" method="POST" class="col-span-1">
-            @csrf
-            <input type="hidden" name="id" value="{{$post->id}}">
-            <input type="submit" value="{{__('Unpublish')}}"
-                   class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-        </form>
-    @endif
-    @if($post->is_deleted)
-        <form action="{{route('posts.restore')}}" method="POST" class="ml-4">
-            @csrf
-            <input type="hidden" name="id" value="{{$post->id}}">
-            <input type="submit" value="{{__('Restore')}}"
-                   class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-        </form>
-    @else
-        <form action="{{route('posts.delete')}}" method="POST" class="ml-4">
-            @csrf
-            <input type="hidden" name="id" value="{{$post->id}}">
-            <input type="submit" value="{{__('Delete')}}"
-                   class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-        </form>
-    @endif
-</div>
 </div>
